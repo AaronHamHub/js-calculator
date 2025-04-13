eecc83faf7bb9353e3460e3a04d224877693b710
Aaron Hammer-Huber <202416678+AaronHamHub@users.noreply.github.com>
2025-04-13 16:34:08 -0400

    operate function created

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function divide(a, b)
     if(b == 0) { throw new Error(); }
     return a / b;
 }
+
+function operate(op, a, b)
+{
+    switch(op)
+    {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            throw new Error();
+    }
+}
